f46e55630c6aca102e9d96a0e7e32e3ae72afdf2
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-04-24 20:08:23 +0700

    feat: create pin message component

--- src/app/react-focus/page.tsx
+++ src/app/react-focus/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import StopwatchTimer from "./_components/StopwatchTimer";
+import PinMessages from "./_components/PinMessage";
 
 const ReactFocusPage = () => {
   return (
     <div>
-      <StopwatchTimer />
+      {/* <StopwatchTimer /> */}
+      <PinMessages />
     </div>
   );
 };
